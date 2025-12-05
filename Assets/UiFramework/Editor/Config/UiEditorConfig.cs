d64edb7e8ac6d186b3f35d88bed1edd290ff79fe
Mohamed Moghazi <eng.moghazy@gmail.com>
2025-12-05 21:38:47 +0100

    resolve: merge UiEditorConfig changes

--- Assets/UiFramework/Editor/Config/UiEditorConfig.cs
+++ Assets/UiFramework/Editor/Config/UiEditorConfig.cs
@@ -5,12 +5,12 @@ namespace UiFramework.Editor.Config
     [CreateAssetMenu(fileName = "UiEditorConfig", menuName = "Scripts/UiFramework/Editor Config")]
     public class UiEditorConfig : ScriptableObject
     {
-        public string UiSetupAssetPath = "Assets/Scripts/UiFramework/Editor/UiSetup.asset";
+        public string UiSetupAssetPath = "Assets/UiConfigs/UiSetup.asset";
         public string ElementsScriptPath = "Assets/Scripts/Ui/UiElements";
         public string ElementsScenePath = "Assets/Scenes/UiElements";
         public string StatesPath = "Assets/Scripts/Ui/UiStates";
-        public string StateRegistryPath = "Assets/Scripts/UiFramework/Editor/UiStateRegistry.asset";
-        public string RuntimeConfigOutputPath = "Assets/Scripts/UiFramework/Runtime/UiConfig.asset";
+        public string StateRegistryPath = "Assets/UiConfigs/UiStateRegistry.asset";
+        public string RuntimeConfigOutputPath = "Assets/UiConfigs/RuntimeUiConfig.asset";
         public string ElementNamespace = "UiFramework.Editor.Elements";
         public string StateNamespace = "UiFramework.Editor.States";
     }
